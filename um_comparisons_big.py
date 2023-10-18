cba67b27b29f6e833a41672d2fa14b306e14ee3b
Sophie Turner <st838@cam.ac.uk>
2023-10-18 12:53:49 +0100

    Finished um comparisons main functionality.

--- um_comparisons_big.py
+++ um_comparisons_big.py
@@ -1,8 +1,9 @@
 '''
 Name: Sophie Turner.
-Date: 12/10/2023.
+Date: 18/10/2023.
 Contact: st838@cam.ac.uk
-Script to compare UKCA data and see differences.
+Simplified script to compare UKCA data and see differences for entire items. Will take a long time to run.
+For a faster version with reduced dimensions see um_comparisons.py.
 Used on Cambridge chemistry department's atmospheric servers. 
 Files are located at scratch/st838/netscratch.
 '''
@@ -73,34 +74,6 @@ def simple_diff(value_1, value_2, data_1_name, data_2_name, values_name, field_n
     print(f'The {values_name} {field_name} values from both datasets are the same.')
 
 
-def plot_diff_2d(data_1, data_2):
-  # Unfinished. Need to allow for different dims and x-y positions. 
-  diff = data_2 - data_1
-  labels = ['Timesteps', 'Altitude / km', 'Latitude / degrees', 'Longitude / degrees']  
-  x_label = labels[2]
-  y_label = labels[1]
-  
-  fig, (ax_1, ax_2, ax_diff) = plt.subplots(nrows=3, ncols=1, figsize=(10,10)) 
-  ax_1.set_title('Original, averaged over timesteps and longitude')
-  ax_2.set_title('Updated, averaged over timesteps and longitude')
-  ax_diff.set_title('Difference') 
-  
-  mesh_1 = ax_1.pcolormesh(data_1)
-  mesh_2 = ax_2.pcolormesh(data_2)
-  for mesh in [mesh_1, mesh_2]:
-    bar = fig.colorbar(mesh)
-    bar.set_label('J HCHO molecular / s\u207b\u00b9')
-  mesh_diff = ax_diff.pcolormesh(diff)
-  bar = fig.colorbar(mesh_diff)
-  bar.set_label('New - old / s\u207b\u00b9')
-  
-  for ax in [ax_1, ax_2, ax_diff]:
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
-  fig.tight_layout(pad=4.0)
-  plt.show() 
-
-
 def diffs(data_1, data_2, name_1, name_2):
   # data1 - data2 to see differences.
   num_values = len(data_1)
@@ -112,24 +85,6 @@ def diffs(data_1, data_2, name_1, name_2):
   print(f'{(num_diff/num_values)*100}% of the data are different.')
   # by how much do they differ?
   view_basics_array(diffs, 'differences')
-  
-  
-def pick_specifics(data):
-  # Unfinished, but this is generally how it will work:
-  data = data[0] # Pick one timestep.
-  data = data[:,11,:,:] # Pick 1 altitude.
-  #data = data[0,:,72,:] # Pick 1 latitude, the equator. 
-  #data = data[0,:,:,60] # Pick 1 longitude.   
-  return(data)
-  
-  
-def zonal_means(data, dims):
-  # dims is a list of True/False for which dims to keep.
-  # False means average over it and remove the dim.
-  for i in range(len(dims)):
-    if not dims[i]:
-      data = np.mean(data, axis=i) # Average over dim.
-  return(data)
   
 
 # File paths.
@@ -148,7 +103,6 @@ code = field[1]
 print('\nComparing', field_name)
 data_1_name = 'old J data'
 data_2_name = 'new J data'
-dims = [False, True, True, False] # Time, alt, lat, long.
 
 print(f'Loading the UM outputs with the {data_1_name}.')
 data_1 = cf.read(file_1,select=code)[0] 
@@ -158,31 +112,11 @@ data_2 = cf.read(file_2,select=code)[0]
 data_1 = np.array(data_1.data)
 data_2 = np.array(data_2.data)
 
-# Avergae and reduce some dims.
-data_1_zones = zonal_means(data_1, dims) 
-data_2_zones = zonal_means(data_1, dims)
-
-# View a 2D plot of the differences.   
-plot_diff_2d(data_1_zones, data_2_zones) 
-del(data_1_zones)
-del(data_2_zones)
-
-# Pick out some points.
-data_1 = data_1[0,0:60,0:108,0:160] # One timestep. Cut off the top altitudes and the portion of space not receiving light.
-data_1 = pick_specifics(data_1)
-data_2 = data_2[0,0:60,0:108,0:160] # One timestep. Cut off the top altitudes and the portion of space not receiving light.
-data_2 = pick_specifics(data_2)
-
 print('Looking at this region:')
 print(f'Time: {data_2.coord("time").data} UTC')
 print(f'Altitude: {data_2.coord("atmosphere_hybrid_height_coordinate").data * 85000} km')
 print('Longitude:', data_2.coord('longitude').data)
 print('Latitude:', data_2.coord('latitude').data)
-
-# Make a plot.
-data_1 = np.squeeze(data_1)
-data_2 = np.squeeze(data_2)
-plot_diff_2d(data_1, data_2)
     
 # See an overview of data and differences.    
 data_1 = data_1.flatten() 
@@ -199,3 +133,6 @@ simple_diff(avg_1, avg_2, data_1_name, data_2_name, 'mean', field_name)
 
 diffs(data_1, data_2, data_1_name, data_2_name)
 print('\n')
+
+
+
